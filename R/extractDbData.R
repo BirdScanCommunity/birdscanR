9ac9ba7cf9e2c619e3dd95d4cb50dde87373d6e9
Birgen Haest <birgen.haest@gmail.com>
2022-05-16 23:27:56 +0200

    Format Code: Increase readability

--- R/extractDbData.R
+++ R/extractDbData.R
@@ -7,7 +7,7 @@
 #' @param dbName NULL The name of the Database
 #' @param dbUser NULL The USER name of the Server
 #' @param dbPwd NULL The password for the user name
-#' @param dbDataDir NULL The path to the output directory where to store the extracted dataset. 
+#' @param dbDataDir NULL The path to the output directory where to store the extracted dataset. If the directory does not exist, it will be created.
 #' @param radarTimeZone NULL String specifying the radar time zone. Default is NULL: extract the timezone from the site table of the sql database.
 #' @param targetTimeZone "Etc/GMT0" String specifying the target time zone. Default is "Etc/GMT0".
 #' @param listOfRfFeaturesToExtract NULL or a list of feature to extract
@@ -39,10 +39,10 @@ extractDbData = function(dbDriverChar              = "SQL Server",
 # =============================================================================
   # CASE: "SQL Server"
   # ===========================================================================
-    if(dbDriverChar == "SQL Server"){
+    if (dbDriverChar == "SQL Server"){
       # CASE: Username and Password are provided
       # =======================================================================
-        if(!is.null(dbUser) | !is.null(dbPwd)){
+        if (!is.null(dbUser) | !is.null(dbPwd)){
           dsn = paste0("driver=", dbDriverChar, ";server=", dbServer,
                        ";database=", dbName,
                        ";uid=", dbUser,
@@ -65,7 +65,7 @@ extractDbData = function(dbDriverChar              = "SQL Server",
     } else if (dbDriverChar == "PostgreSQL"){
       # CASE: Username and Password are provided
       # =======================================================================
-        if(!is.null(dbUser) | !is.null(dbPwd)){
+        if (!is.null(dbUser) | !is.null(dbPwd)){
           dbConnection = DBI::dbConnect("PostgreSQL",
                                         host     = "cloud.birdradar.com",
                                         dbname   = dbName,
@@ -83,11 +83,10 @@ extractDbData = function(dbDriverChar              = "SQL Server",
                                          password = rstudioapi::askForPassword("Database password"))
        }
     }
-      
-      
+ 
 # Start extracting data from sql database
 # =============================================================================
-  if(if(dbDriverChar == "PostgreSQL"){isPostgresqlIdCurrent(dbConnection)} else {dbConnection != -1}){
+  if (if (dbDriverChar == "PostgreSQL"){isPostgresqlIdCurrent(dbConnection)} else {dbConnection != -1}){
     # load collection table
     # =========================================================================
       message("Extracting collection table from DB...")
@@ -119,7 +118,7 @@ extractDbData = function(dbDriverChar              = "SQL Server",
     # =========================================================================
       message("Extracting MANUAL visibility table from DB...")
       manualVisibilityTable = try(getManualVisibilityTable(dbConnection, dbDriverChar), silent = TRUE)
-      if(is.data.frame(manualVisibilityTable)){  
+      if (is.data.frame(manualVisibilityTable)){  
         message("MANUAL visibility table extracted")
       } else {
         message("MANUAL visibility table not in DB")
@@ -131,134 +130,171 @@ extractDbData = function(dbDriverChar              = "SQL Server",
       message("Extracting time_bins table from DB...")
       timeBinsTable = getTimeBinsTable(dbConnection, dbDriverChar)
     
-    # :::::::::::::::::::::::::::::::::::::::::::::::::::::::
     # load weather from local MS-SQL DB
-    message("Extracting weather table from DB...")
-    weatherTable = QUERY(dbConnection, dbDriverChar, 
-                         "Select * From weather"
-    )
+    # =========================================================================
+      message("Extracting weather table from DB...")
+      weatherTable = QUERY(dbConnection, dbDriverChar, "Select * From weather")
     
-    # :::::::::::::::::::::::::::::::::::::::::::::::::::::::
     # load weather properties from local MS-SQL DB
-    message("Extracting weather_property table from DB...")
-    weatherPropertyTable = QUERY(dbConnection, dbDriverChar,  
-                                 "Select * From weather_property"
-    )
+    # =========================================================================
+      message("Extracting weather_property table from DB...")
+      weatherPropertyTable = QUERY(dbConnection, dbDriverChar, 
+                                   "Select * From weather_property")
+      weatherPropertyList = weatherTable$weather_property
+      weatherTable$weather_property = weatherPropertyTable$property_name[match(weatherPropertyList, 
+                                                                               weatherPropertyTable$id)]
+      weather = reshape2::dcast(weatherTable, 
+                                time_bin ~ weather_property, 
+                                value.var     = "value", 
+                                fun.aggregate = mean)
+      rm(list = "weatherTable", "weatherPropertyTable", "weatherPropertyList")
     
-    weatherPropertyList = weatherTable$weather_property
-    weatherTable$weather_property = weatherPropertyTable$property_name[ match(weatherPropertyList, weatherPropertyTable$id) ]
-    weather = reshape2::dcast(weatherTable, time_bin ~ weather_property, value.var = "value", fun.aggregate = mean)
-    rm(list = "weatherTable", "weatherPropertyTable", "weatherPropertyList")
-    
-    # :::::::::::::::::::::::::::::::::::::::::::::::::::::::
     # get all listed rf features
-    message("Extracting rffeatures table from DB...")
-    echoRfFeatureMap = getEchoFeatures(dbConnection, dbDriverChar, listOfRfFeaturesToExtract = listOfRfFeaturesToExtract)   
+    # =========================================================================
+      message("Extracting rffeatures table from DB...")
+      echoRfFeatureMap = getEchoFeatures(dbConnection, dbDriverChar, 
+                                         listOfRfFeaturesToExtract = listOfRfFeaturesToExtract)   
     
-    # :::::::::::::::::::::::::::::::::::::::::::::::::::::::
     # load rf classification
-    message("Extracting RF classification...")
-    rfclassificationTable = getRfClassification(dbConnection, dbDriverChar)
+    # =========================================================================
+      message("Extracting RF classification...")
+      rfclassificationTable = getRfClassification(dbConnection, dbDriverChar)
     
-    # :::::::::::::::::::::::::::::::::::::::::::::::::::::::
     # load echo validation from local MS-SQL DB
-    message("Extracting echo_validation table from DB...")
-    echovalidationTable = getEchoValidationTable(dbConnection, dbDriverChar)
+    # =========================================================================
+      message("Extracting echo_validation table from DB...")
+      echovalidationTable = getEchoValidationTable(dbConnection, dbDriverChar)
     
     # Merge echo Data
-    echoData = collectionTable
-    names(echoData)[ names(echoData) == "row"] = "echo"
-    if(!is.null(echoRfFeatureMap$echoRfFeatureMap))
-    {
-       echoData = merge(echoData, echoRfFeatureMap$echoRfFeatureMap, by = "echo", all.x = TRUE, all.y = FALSE)      
-    }
-    echoData = merge(echoData, echovalidationTable, by = "echo", all.x = TRUE, all.y = FALSE)
-    echoData = merge(echoData, rfclassificationTable$rfclassificationTable, by = "echo", all.x = TRUE, all.y = FALSE)
-    availableClasses = rfclassificationTable$availableClasses
-    classProbabilitiesAndMtrFactors = rfclassificationTable$classProbabilitiesAndMtrFactors
-    rfFeatures = echoRfFeatureMap$rfFeatures
-    rm(collectionTable, echoRfFeatureMap, echovalidationTable, rfclassificationTable)
+    # =========================================================================
+      echoData = collectionTable
+      names(echoData)[names(echoData) == "row"] = "echo"
+      if (!is.null(echoRfFeatureMap$echoRfFeatureMap)){
+        echoData = merge(echoData, 
+                         echoRfFeatureMap$echoRfFeatureMap, 
+                         by = "echo", all.x = TRUE, all.y = FALSE)
+      }
+      echoData = merge(echoData, echovalidationTable, 
+                       by = "echo", all.x = TRUE, all.y = FALSE)
+      echoData = merge(echoData, rfclassificationTable$rfclassificationTable, 
+                       by = "echo", all.x = TRUE, all.y = FALSE)
+      availableClasses = rfclassificationTable$availableClasses
+      classProbabilitiesAndMtrFactors = rfclassificationTable$classProbabilitiesAndMtrFactors
+      rfFeatures                      = echoRfFeatureMap$rfFeatures
+      rm(collectionTable, echoRfFeatureMap, 
+         echovalidationTable, rfclassificationTable)
     
     # rename protocolTable
-    protocolData = protocolTable
-    rm(protocolTable)
+    # =========================================================================
+      protocolData = protocolTable
+      rm(protocolTable)
     
     # Merge site Data
-    siteData = merge(siteTable, radarTable, by = "radarID", all = TRUE)
-    rm(siteTable, radarTable)
+    # =========================================================================
+      siteData = merge(siteTable, radarTable, by = "radarID", all = TRUE)
+      rm(siteTable, radarTable)
     
     # Merge timebin Data
-    timeBinData = timeBinsTable
-    names(timeBinData)[ names(timeBinData) == "id"] = "time_bin"
-    timeBinData = merge(timeBinData, weather, by = "time_bin", all.x =TRUE, all.y = FALSE)
-    rm(timeBinsTable, weather)
+    # =========================================================================
+      timeBinData = timeBinsTable
+      names(timeBinData)[names(timeBinData) == "id"] = "time_bin"
+      timeBinData = merge(timeBinData, weather, 
+                          by = "time_bin", all.x =TRUE, all.y = FALSE)
+      rm(timeBinsTable, weather)
      
-     # insert a.s.l. altitude column to echoData
-     asl = data.frame("feature1.altitude_ASL" = echoData$feature1.altitude_AGL) + siteData$altitude
-     echoData = data.frame(echoData[ , 1:match("feature1.altitude_AGL", names(echoData)) ], asl, echoData[ , (match("feature1.altitude_AGL", names(echoData)) + 1) : length(echoData) ])
-     rm(asl)
+    # insert a.s.l. altitude column to echoData
+    # =========================================================================
+      asl      = data.frame("feature1.altitude_ASL" = echoData$feature1.altitude_AGL) + 
+                  siteData$altitude
+      echoData = data.frame(echoData[, 1:match("feature1.altitude_AGL", names(echoData))], 
+                            asl, 
+                            echoData[, (match("feature1.altitude_AGL", names(echoData))+1):length(echoData)])
+      rm(asl)
      
-     # get radarTZ from siteData (or siteTable)
-     if(is.null(radarTimeZone)) {
+    # get radarTZ from siteData (or siteTable)
+    # =========================================================================
+      if (is.null(radarTimeZone)){
         tz_shift = as.numeric(siteData$timeShift) # Get time zone saved in the database table 'dbo.site'
-        if(is.na(tz_shift) | is.null(tz_shift)) stop("set a radarTimeZone, or update the timeshift column in the dbo-site table")
-        if(tz_shift >= 0 | tz_shift < 0){
-           radarTimeZone = paste0("Etc/GMT", ifelse(tz_shift >=0 , "-", "+"), abs(tz_shift)) # note that "UTC+1" is denoted as "Etc/GMT-1"
-           message(paste0("Radar timezone extracted from dbo.site is :", radarTimeZone))
+          
+        if (is.na(tz_shift) | is.null(tz_shift)){stop("set a radarTimeZone, or update the timeshift column in the dbo-site table")}
+        if (tz_shift >= 0 | tz_shift < 0){
+          radarTimeZone = paste0("Etc/GMT", ifelse(tz_shift >=0 , "-", "+"), abs(tz_shift)) # note that "UTC+1" is denoted as "Etc/GMT-1"
+          message(paste0("Radar timezone extracted from dbo.site is :", radarTimeZone))
         }
-     }
-     TimeZone = data.frame("radarTimeZone" = radarTimeZone,
+      }
+      TimeZone = data.frame("radarTimeZone"  = radarTimeZone,
                             "targetTimeZone" = targetTimeZone)
-     # timezone conversion
-     visibilityData = convertTimeZone(data = visibilityData, colNames = c("blind_from", "blind_to"), originTZ = radarTimeZone, targetTZ = targetTimeZone)
-     protocolData = convertTimeZone(data = protocolData, colNames = c("startTime", "stopTime"), originTZ = radarTimeZone, targetTZ = targetTimeZone)
-     siteData = convertTimeZone(data = siteData, colNames = c("projectStart", "projectEnd"), originTZ = radarTimeZone, targetTZ = targetTimeZone)
-     timeBinData = convertTimeZone(data = timeBinData, colNames = c("time_start", "time_stop"), originTZ = radarTimeZone, targetTZ = targetTimeZone)
-     echoData = convertTimeZone(data = echoData, colNames = c("time_stamp"), originTZ = radarTimeZone, targetTZ = targetTimeZone)
+       
+    # timezone conversion
+    # =========================================================================
+      visibilityData = convertTimeZone(data     = visibilityData, 
+                                       colNames = c("blind_from", "blind_to"), 
+                                       originTZ = radarTimeZone, 
+                                       targetTZ = targetTimeZone)
+      protocolData   = convertTimeZone(data     = protocolData, 
+                                       colNames = c("startTime", "stopTime"), 
+                                       originTZ = radarTimeZone, 
+                                       targetTZ = targetTimeZone)
+      siteData       = convertTimeZone(data     = siteData, 
+                                       colNames = c("projectStart", "projectEnd"), 
+                                       originTZ = radarTimeZone, 
+                                       targetTZ = targetTimeZone)
+      timeBinData    = convertTimeZone(data     = timeBinData, 
+                                       colNames = c("time_start", "time_stop"), 
+                                       originTZ = radarTimeZone, 
+                                       targetTZ = targetTimeZone)
+      echoData       = convertTimeZone(data     = echoData, 
+                                       colNames = c("time_stamp"), 
+                                       originTZ = radarTimeZone, 
+                                       targetTZ = targetTimeZone)
+          
+    # Create directory if not existing
+    # =========================================================================
+      dir.create(dbDataDir, showWarnings = F, recursive = T)
      
-     # Create directory if not existing
-     ifelse(!dir.exists(dbDataDir), dir.create(dbDataDir), FALSE)
-     
-     # save DB Data
-     if(exists("manualVisibilityTable")){  
+    # save DB Data
+    # =========================================================================
+      if (exists("manualVisibilityTable")){  
         save(echoData, 
-              protocolData,
-              siteData, 
-              visibilityData,
-              manualVisibilityTable,
-              timeBinData,
-              availableClasses,
-              rfFeatures,
-              TimeZone,
-              classProbabilitiesAndMtrFactors,
-              file = file.path(dbDataDir, dbDataName))
-     } else {
+             protocolData,
+             siteData, 
+             visibilityData,
+             manualVisibilityTable,
+             timeBinData,
+             availableClasses,
+             rfFeatures,
+             TimeZone,
+             classProbabilitiesAndMtrFactors,
+             file = file.path(dbDataDir, dbDataName))
+      } else {
         save(echoData, 
-              protocolData,
-              siteData, 
-              visibilityData,
-              timeBinData,
-              availableClasses,
-              rfFeatures,
-              TimeZone,
-              classProbabilitiesAndMtrFactors,
-              file = file.path(dbDataDir, dbDataName))
+             protocolData,
+             siteData, 
+             visibilityData,
+             timeBinData,
+             availableClasses,
+             rfFeatures,
+             TimeZone,
+             classProbabilitiesAndMtrFactors,
+             file = file.path(dbDataDir, dbDataName))
      }
      
      
      # close database connections
-     if(dbDriverChar != "PostgreSQL") {
-        RODBC::odbcCloseAll() 
-     } else {
-        DBI::dbDisconnect(dbConnection)
-     }      
+     # =========================================================================
+       if (dbDriverChar != "PostgreSQL") {
+          RODBC::odbcCloseAll() 
+       } else {
+          DBI::dbDisconnect(dbConnection)
+       }      
+      
   } else {
     warning("Could not open database. Make sure to set dbServer, dbName and credentials right.")
   }
 
 # Return output
 # =============================================================================
-if(exists("manualVisibilityTable")){  
+if (exists("manualVisibilityTable")){  
   return(list(echoData                        = echoData, 
               protocolData                    = protocolData,
               siteData                        = siteData, 
