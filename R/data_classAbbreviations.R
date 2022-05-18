3e0aeb8b65c03c667fe81f10edca4e876727b5be
Birgen Haest <birgen.haest@gmail.com>
2022-05-18 17:11:09 +0200

    Include classAbbreviations as data in the package

---
+++ R/data_classAbbreviations.R
@@ -0,0 +1,16 @@
+#' Defaul class abbreviations table of the birdscanR package
+#'
+#' Table to allow for easy abbreviations of the standard classes of the Birdscan
+#' MR1. 
+#'
+#' @docType data
+#'
+#' @usage data(classAbbreviations)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(classAbbreviations)
+"classAbbreviations"
