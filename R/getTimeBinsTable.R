e61e4e415eb3499d640b1901a92ee8f16521bb3d
Birgen Haest <birgen.haest@gmail.com>
2022-10-24 15:52:18 +0200

    Nothing substantial; formatting only

--- R/getTimeBinsTable.R
+++ R/getTimeBinsTable.R
@@ -1,31 +1,38 @@
-#### getTimeBinsTable ------------------------------------------------------------
+#### getTimeBinsTable ----------------------------------------------------------
 #' @title  Get BirdScan timebins table
 #' @description load timebins table from an already connect local MS-SQL DB
-#' @author Fabian Hertner (SBRS) \email{fabian.hertner@@swiss-birdradar.com}
+#' @author Fabian Hertner (SBRS) \email{fabian.hertner@@swiss-birdradar.com}; with edits by Birgen Haest, \email{birgen.haest@@vogelwarte.ch}
 #' @param dbConnection a valid database connection
 #' @param dbDriverChar the name of the driver. If different from 'PostgreSQL' it connects to cloud.birdradar.com
 #'
 #' @return A dataframe with the timebins table
 #' @export
 #'
-getTimeBinsTable = function( dbConnection, dbDriverChar )
-{
-   # :::::::::::::::::::::::::::::::::::::::::::::::::::::::
-   # load protocol table from local MS-SQL DB
-   if(dbDriverChar != 'PostgreSQL') {
-      timeBinsTable = QUERY(dbConnection, dbDriverChar, "Select * From time_bins order by id asc")
-      timeBinsTable_times = QUERY(dbConnection, dbDriverChar,"Select time_start, time_stop From time_bins order by id asc", as.is = TRUE)
-      timeBinsTable$time_start <- timeBinsTable_times$time_start
-      timeBinsTable$time_stop <- timeBinsTable_times$time_stop
-   } else {
-      timeBinsTable = QUERY(dbConnection, dbDriverChar, "Select *,time_start::character varying as start,time_stop::character varying as stop FROM time_bins order by id asc")
-      timeBinsTable$time_start <- timeBinsTable$start
-      timeBinsTable$time_stop <- timeBinsTable$stop
-      timeBinsTable$start <- NULL
-      timeBinsTable$stop <- NULL
-   }
-   
-   colnames(timeBinsTable)[colnames(timeBinsTable) == "siteid"] <- "siteID"
-   
-   return( timeBinsTable )
+getTimeBinsTable = function(dbConnection, dbDriverChar){
+  # load protocol table from local MS-SQL DB
+  # ==============================================================================
+    if (dbDriverChar != 'PostgreSQL'){
+      timeBinsTable            = QUERY(dbConnection, dbDriverChar, 
+                                       "Select * From time_bins order by id asc")
+      timeBinsTable_times      = QUERY(dbConnection, 
+                                       dbDriverChar, 
+                                       "Select time_start, time_stop From time_bins order by id asc", 
+                                       as.is = TRUE)
+      timeBinsTable$time_start = timeBinsTable_times$time_start
+      timeBinsTable$time_stop  = timeBinsTable_times$time_stop
+  
+  # load protocol table from PostGreSQL
+  # ==============================================================================
+    } else {
+      timeBinsTable            = QUERY(dbConnection, 
+                                       dbDriverChar, 
+                                       "Select *,time_start::character varying as start,time_stop::character varying as stop FROM time_bins order by id asc")
+      timeBinsTable$time_start = timeBinsTable$start
+      timeBinsTable$time_stop  = timeBinsTable$stop
+      timeBinsTable$start      = NULL
+      timeBinsTable$stop       = NULL
+    }
+  
+  colnames(timeBinsTable)[colnames(timeBinsTable) == "siteid"] = "siteID"
+  return(timeBinsTable)
 }
