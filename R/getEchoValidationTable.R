39b7b06843e69ebc43040aa762313fee2681d0b3
Birgen Haest <birgen.haest@gmail.com>
2022-10-24 13:09:22 +0200

    Nothing substantial; formatting only

--- R/getEchoValidationTable.R
+++ R/getEchoValidationTable.R
@@ -1,27 +1,26 @@
 #### getEchoValidationTable ------------------------------------------------------------
 #' @title  Get a BirdScan echo validation table
 #' @description  gets the EchoValidationTable from an already connected DB
-#' @author Fabian Hertner (SBRS) \email{fabian.hertner@@swiss-birdradar.com}
+#' @author Fabian Hertner (SBRS) \email{fabian.hertner@@swiss-birdradar.com}; with edits by Birgen Haest, \email{birgen.haest@@vogelwarte.ch}
 #' @param dbConnection a valid  database connection
 #'
 #' @return A dataframe called echovalidationTable
 #' @export
 #'
-getEchoValidationTable = function( dbConnection, dbDriverChar )
-{
-   echovalidationTypesTable = QUERY(dbConnection, dbDriverChar, 
-                                    "Select * From echo_validation_type"
-   )
+getEchoValidationTable = function(dbConnection, dbDriverChar){
+  echovalidationTypesTable = QUERY(dbConnection, 
+                                   dbDriverChar, 
+                                   "Select * From echo_validation_type")
    
-   echovalidationTable = QUERY(dbConnection, dbDriverChar, 
-                               "Select * From echo_validation order by echo_id asc"
-   )
+  echovalidationTable = QUERY(dbConnection, 
+                              dbDriverChar, 
+                              "Select * From echo_validation order by echo_id asc")
    
-   echoValidationList <- echovalidationTable$type
-   echovalidationTable$type <- echovalidationTypesTable$name[ match( echoValidationList, echovalidationTypesTable$id ) ]
-   rm( list = "echovalidationTypesTable", "echoValidationList" )
-   names( echovalidationTable )[ names( echovalidationTable ) == "echo_id"] <- "echo"
-   names( echovalidationTable )[ names( echovalidationTable ) == "type"] <- "echoValidationType"
+  echoValidationList       = echovalidationTable$type
+  echovalidationTable$type = echovalidationTypesTable$name[match(echoValidationList, echovalidationTypesTable$id)]
+  rm(list = "echovalidationTypesTable", "echoValidationList")
+  names(echovalidationTable)[names(echovalidationTable) == "echo_id"] = "echo"
+  names(echovalidationTable)[names(echovalidationTable) == "type"]    = "echoValidationType"
    
-   return( echovalidationTable )
+  return(echovalidationTable)
 }
