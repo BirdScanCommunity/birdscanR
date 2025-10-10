603f626e0ebd25f0d2d56b7e572c1a92fd511fd9
BirgenH <BirgenH@users.noreply.github.com>
2025-10-10 20:31:46 +0000

    Style code (GHA)

--- R/getSiteTable.R
+++ R/getSiteTable.R
@@ -10,25 +10,25 @@
 #' \dontrun{
 #' # Set server and database settings
 #' # ==========================================================================
-#'   # Using and Microsoft SQL database
-#'   # ========================================================================
-#'     dbServer     = "MACHINE\\SERVERNAME" # Set the name of your SQL server
-#'     dbName       = "db_Name"             # Set the name of your database
-#'     dbDriverChar = "SQL Server"          # Set to "SQL Server"
+#' # Using and Microsoft SQL database
+#' # ========================================================================
+#' dbServer = "MACHINE\\SERVERNAME" # Set the name of your SQL server
+#' dbName = "db_Name" # Set the name of your database
+#' dbDriverChar = "SQL Server" # Set to "SQL Server"
 #'
-#'   # Using a PostgreSQL
-#'   # ========================================================================
-#'     dbServer     = "cloud.birdradar.com" # Set the name or IP of your postgreSQL
-#'     dbName       = "db_Name"             # Set the name of your database
-#'     dbDriverChar = "PostgreSQL"          # Set to "PostgreSQL"
+#' # Using a PostgreSQL
+#' # ========================================================================
+#' dbServer = "cloud.birdradar.com" # Set the name or IP of your postgreSQL
+#' dbName = "db_Name" # Set the name of your database
+#' dbDriverChar = "PostgreSQL" # Set to "PostgreSQL"
 #'
 #' # Open the connection with the database
 #' # ==========================================================================
-#'   dbConnection = dbConnectBirdscanSQL(
-#'                    dbDriverChar = dbDriverChar,
-#'                    dbServer     = dbServer,
-#'                    dbName       = dbName,
-#'   )
+#' dbConnection = dbConnectBirdscanSQL(
+#'   dbDriverChar = dbDriverChar,
+#'   dbServer     = dbServer,
+#'   dbName       = dbName,
+#' )
 #'
 #' siteTable <- getSiteTable(dbConnection)
 #' }
